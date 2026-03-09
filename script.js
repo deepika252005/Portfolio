e3ce9973d770b7ea8ed96f535709fbbdc7018349
deepika252005 <deepikagovindharaj2005@gmail.com>
2026-03-09 14:47:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+ var viewportfolio =(document.getElementsByquerySelector(".view"))
+
+viewportfolio.addEventListener("click", function(){
+     window.location.href="mailto:deepikagovindharaj2005@gmail.com";
+     
+})
+     
